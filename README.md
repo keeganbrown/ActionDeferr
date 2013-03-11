33a1a1eab10ffc5da4e1d49dfcc49581c565f971
Keegan Brown <brownkg@peteramayer.com>
2013-03-11 10:16:21 -0500

    Added actionDeferr.js
    Inital Commit. I've got some plans for updates soon, but for now this
    library works for my usecases.

---
+++ README.md
@@ -0,0 +1,10 @@
+ActionDeferr.js
+===================
+
+Are tracking or other odd DOM manupulations messing with your animation?
+Not anymore! Now, there's actionDeferr.js!
+
+In all seriousness, all this simple library does is add functions to a queue, and then execute those functions when there is no animation taking place. The reason I made it is simple: DOM Manipulation stops all browser paint-events (a very common side-effect of visitor tracking).
+
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ actionDeferr.js
@@ -0,0 +1,67 @@
+function ActionDeferr (_allowDebug) {
+	
+	var actionQueue = [];
+	var actionDeferr = null;
+	var elementWatchList = "html,body,div,span,li,a";
+	var allowDebug = _allowDebug;
+	
+	function DefferedAction ( args, callback ) {
+		this.args = args;
+		this.callback = callback;
+		DefferedAction.prototype.execute = function () {
+			if ( !!this.callback && !!this.callback.apply ) {
+				this.callback.apply( window, this.args );
+				!!allowDebug && !!window.console && console.log("deferred: " + callback + " -- " + this.args.join("|") );
+			}
+		}
+	}
+	function isAnythingAnimating () {
+		var v = false;
+		//CHECK FOR ANIMATING JQUERY ELEMENTS
+		if ( !!jQuery && jQuery(elementWatchList).is(":animated") ) {
+			v = true;
+			return v;
+		}
+		//CHECK FOR ANIMATING TWEENLITE/TWEENMAX ELEMENTS
+		var GS = null;
+		if ( !!window.TweenMax ) { GS = TweenMax }
+		if ( !!window.TweenLite ) { GS = TweenLite }
+		if ( !GS ) { return v; }
+		var tweens = TweenMax.getAllTweens();
+		for (var i = 0, len = tweens.length; i < len; i++ ) {
+			if ( !tweens[i].paused ) {
+				v = true;
+				return v;
+			}
+		}
+		return v;
+	}
+	function executeQueue() {
+		if ( !!actionQueue.length ) {
+			if( !isAnythingAnimating() ) {
+				while ( !!actionQueue.length ) {
+					actionQueue.pop().execute();
+				}
+			} else {
+				if ( !!actionDeferr ) {
+					clearTimeout( actionDeferr );
+					actionDeferr = null;
+				}
+				actionDeferr = setTimeout( deferredHandle, 333 );
+			}
+		}
+	}
+	function deferredHandle () {
+		actionDeferr = null;
+		executeQueue();
+	}
+	ActionDeferr.prototype.enqueueAction = function enqueueAction( callback, args ) {
+		actionQueue.push( new DefferedAction( args, callback ) );
+		if ( !!actionDeferr ) {
+			clearTimeout( actionDeferr );
+			actionDeferr = null;
+		}
+		actionDeferr = setTimeout( deferredHandle, 333 );
+	}
+}
+var Deferr = new ActionDeferr(true);
